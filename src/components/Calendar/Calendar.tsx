883fdc74b24107ff096b48aed42061c3748b35ee
Andrew Brough <andrew.brough@fullscript.com>
2021-11-17 17:50:38 -0500

    some basic styling

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,6 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 import { FC } from "react";
+import { colors } from "../../variables/colors";
 import { CalendarDays } from "./CalendarDays";
 import { CalendarTitle } from "./CalendarTitle";
 import { CalendarProvider } from "./useCalendarContext";
@@ -8,7 +9,11 @@ import { CalendarProvider } from "./useCalendarContext";
 const calendar = css`
   width: 100%;
   display: grid;
-  grid-template-columns: repeat(7, calc(100% / 7));
+  grid-template-columns: repeat(7, 1fr);
+  gap: 2px;
+  background: ${colors.primary};
+  border: 2px solid ${colors.primary};
+  box-sizing: border-box;
 `;
 
 /**
--- src/components/Calendar/CalendarDay.tsx
+++ src/components/Calendar/CalendarDay.tsx
@@ -3,10 +3,13 @@ import { css } from "@emotion/react";
 import { FC } from "react";
 
 const calendarDay = css`
-  height: calc(100vw / 7);
-  &:nth-of-type(2n + 1) {
+  height: calc(100vw / 7 - 0.125rem);
+  background: white;
+  padding: 0.5rem;
+  /* &:nth-of-type(2n + 1) {
     background: whitesmoke;
-  }
+  } */
+  box-sizing: border-box;
 `;
 
 export const CalendarDay: FC = (props) => {
